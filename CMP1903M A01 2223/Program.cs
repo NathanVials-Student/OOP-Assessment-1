4fe53fb57615bb23360a29895fdfae3cab52af6c
Earl Smalley <16617508@students.lincoln.ac.uk>
2023-03-07 09:42:57 +0000

    Resolved compilation and runtime errors

--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace CMP1903M_A01_2223
 {
-    class Program
+    class Testing
     {
         static void Main(string[] args)
         {
+            //test pack initialisation
+            Pack pack = new Pack();
+
+            //test first shuffle
+            Pack.shuffleCardPack(0);
+
+            //test second shuffle
+            Pack.shuffleCardPack(1);
+
+            //test one card deal
+            Pack.deal();
+
+            //test several card deal
+            Pack.dealCard(3);
 
         }
     }
